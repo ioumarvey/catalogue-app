2efb02d9857159efbe893b3b309ebb09c156696c
sal <96798879+ioumarvey@users.noreply.github.com>
2023-07-08 23:45:11 +0200

    cs fixer error fixed

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -72,8 +72,9 @@ class TaskController extends AbstractController
      * @return Response HTTP response
      */
     #[Route('/{id}/add_favorite', name: 'task_add_favorite', methods: 'GET|POST')]
-	public function createFavorite(Request $request, Task $task): Response
-{        $form = $this->createForm(
+    public function createFavorite(Request $request, Task $task): Response
+    {
+        $form = $this->createForm(
             FormType::class,
             $task,
             [
@@ -154,7 +155,187 @@ class TaskController extends AbstractController
      *
      * @return Response HTTP response
      */
-    #[Route('/{id}', name: 'task_show', requirements: ['id' => '[1-9]\d*'], methods: 'GET')]
+    #[Route('/{id}', name: 'task_show', requirements: ['id' => '@PSR1,@PSR2,@PSR12'], methods: 'GET')]
+    public function show(Task $task): Response
+    {
+        return $this->render('task/show.html.twig', ['task' => $task]);
+    }
+
+    /**
+     * Show favorited action.
+     *
+     * @param Request $request HTTP Request
+     *
+     * @return Response HTTP response
+     */
+    #[Route('/favorite', name: 'task_favorited', methods: 'GET')]
+    public function favorited(Request $request): Response
+    {
+        $filters = $this->getFilters($request);
+        /**
+         * @var User $user
+         */
+       Here are the corrected spacing in the given file:
+
+```php
+<?php
+/**
+ * Task controller.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Entity\Comment;
+use App\Entity\GuestUser;
+use App\Form\Type\TaskType;
+use App\Form\Type\CommentType;
+use App\Service\CommentService;
+use App\Entity\User;
+use App\Service\TaskServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * Class TaskController.
+ */
+#[Route('/task')]
+class TaskController extends AbstractController
+{
+    /**
+     * Constructor.
+     *
+     * @param TaskServiceInterface $taskService    Task service
+     * @param TranslatorInterface  $translator     Translator
+     * @param CommentService       $commentService Comment service
+     */
+    public function __construct(TaskServiceInterface $taskService, TranslatorInterface $translator, CommentService $commentService)
+    {
+        $this->taskService = $taskService;
+        $this->translator = $translator;
+        $this->commentService = $commentService;
+    }
+
+    /**
+     * Index action.
+     *
+     * @param Request $request HTTP Request
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        name: 'task_index',
+        methods: 'GET'
+    )]
+    public function index(Request $request): Response
+    {
+        $filters = $this->getFilters($request);
+        $pagination = $this->taskService->getPaginatedList(
+            $request->query->getInt('page', 1),
+            $filters
+        );
+
+        return $this->render('task/index.html.twig', ['pagination' => $pagination]);
+    }
+
+    /**
+     * Add favorite action.
+     *
+     * @param Request $request HTTP request
+     * @param Task    $task    Task entity
+     *
+     * @return Response HTTP response
+     */
+    #[Route('/{id}/add_favorite', name: 'task_add_favorite', methods: 'GET|POST')]
+    public function createFavorite(Request $request, Task $task): Response
+    {
+        $form = $this->createForm(
+            FormType::class,
+            $task,
+            [
+                'action' => $this->generateUrl('task_add_favorite', ['id' => $task->getId()]),
+            ]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            $task->addFavorited($user);
+            $this->taskService->save($task);
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans('message.added_successfully')
+            );
+
+            return $this->redirectToRoute('task_favorited');
+        }
+
+        return $this->render(
+            'favorite/add.html.twig',
+            [
+                'form' => $form->createView(),
+                'task' => $task,
+            ]
+        );
+    }
+
+    /**
+     * Delete favorite action.
+     *
+     * @param Request $request HTTP request
+     * @param Task    $task    Task entity
+     *
+     * @return Response HTTP response
+     */
+    #[Route('/{id}/delete_favorite', name: 'task_delete_favorite', requirements: ['id' => '[1-9]\d*'], methods: 'GET|DELETE')]
+    public function deleteFavorite(Request $request, Task $task): Response
+    {
+        $form = $this->createForm(
+            FormType::class,
+            $task,
+            [
+                'method' => 'DELETE',
+                'action' => $this->generateUrl('task_delete_favorite', ['id' => $task->getId()]),
+            ]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            $task->removeFavorited($user);
+            $this->taskService->save($task);
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans('message.deleted_successfully')
+            );
+
+            return $this->redirectToRoute('task_favorited');
+        }
+
+        return $this->render(
+            'favorite/delete.html.twig',
+            [
+                'form' => $form->createView(),
+                'task' => $task,
+            ]
+        );
+    }
+
+    /**
+     * Show action.
+     *
+     * @param Task $task Task entity
+     *
+     * @return Response HTTP response
+     */
+    #[Route('/{id}', name: 'task_show', requirements: ['id' => '@PSR1,@PSR2,@PSR12'], methods: 'GET')]
     public function show(Task $task): Response
     {
         return $this->render('task/show.html.twig', ['task' => $task]);
@@ -192,7 +373,7 @@ class TaskController extends AbstractController
      * @return Response HTTP response
      */
     #[Route(
-        '/create',
+       '/create',
         name: 'task_create',
         methods: 'GET|POST',
     )]
@@ -318,7 +499,7 @@ class TaskController extends AbstractController
      * View action.
      *
      * @param Request $request HTTP request
-     * @param ID      $id      id
+     * @param int     $id      Task ID
      *
      * @return Response HTTP response
      */
@@ -330,12 +511,12 @@ class TaskController extends AbstractController
     )]
     public function view(Request $request, int $id): Response
     {
-        $task = $this->taskRepository->find($id);
+        $task = $this->taskService->getTaskById($id);
         $pagination = $this->commentService->getPaginatedList($request->query->getInt('page', 1));
 
         $comment = new Comment();
         $comment->setTask($task);
-        $comment->setUser($this->tokenStorage->getToken()->getUser());
+        $comment->setUser($this->getUser());
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
 
@@ -358,8 +539,6 @@ class TaskController extends AbstractController
      * @param Request $request HTTP request
      *
      * @return array<string, int> Array of filters
-     *
-     * @psalm-return array{category_id: int, tag_id: int, status_id: int}
      */
     private function getFilters(Request $request): array
     {
